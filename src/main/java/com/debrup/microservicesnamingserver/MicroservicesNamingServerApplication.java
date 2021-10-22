28a6303dbff148fd71e0640c772dca95e9dab85b
Debrup Mondal <debrup.official.365@gmail.com>
2021-10-22 12:15:22 +0530

    application initialized

---
+++ src/main/java/com/debrup/microservicesnamingserver/MicroservicesNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.debrup.microservicesnamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesNamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesNamingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/debrup/microservicesnamingserver/MicroservicesNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debrup.microservicesnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesNamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
